ff44f69982ad2dad6eec08115ed5266efc8de171
SetScript <101998415+SetScript@users.noreply.github.com>
2023-03-07 23:31:55 +0300

    Add files via upload

---
+++ Raccoon/commands.rb
@@ -0,0 +1,28 @@
+def bring message
+      puts message
+end
+  #write message in console
+
+def wait time
+  sleep time
+end
+  #wait on `time` seconds
+
+def input
+  gets
+end
+  #gets
+
+
+def random(num)
+  Random.rand(num)
+end
+  #random
+def int var
+  var
+end
+
+def arr array
+  array = []
+end
+  #idk, but ok
